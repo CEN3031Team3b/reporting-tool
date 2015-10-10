c20f6e72a23d088e0afe04c1f39676a5ac8a4950
Noah@Noah <noahpresser@ufl.edu>
2015-10-10 14:42:09 -0400

    Routing and views for Brand, SKU

---
+++ modules/core/client/controllers/SKU.client.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('core').controller('SKUController', ['$scope', 'Authentication',
+  function ($scope, Authentication) {
+    // This provides Authentication context.
+    $scope.authentication = Authentication;
+  }
+]);
+
+var app = angular.module('myApp', []);
+angular.module('core').controller('customersCtrl', function($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function (response) {$scope.names = response.records;});
+});
+
+
+angular.module('core').controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
