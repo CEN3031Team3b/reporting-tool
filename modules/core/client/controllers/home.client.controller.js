1c4283efac1a454b1f536b65edc1fad5e684691e
reyjr6 <reyolivojr@hotmail.com>
2015-10-10 13:17:17 -0400

    Initial dashboard changes
    Still using irrelevant data

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,3 +6,22 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     $scope.authentication = Authentication;
   }
 ]);
+
+var app = angular.module('myApp', []);
+angular.module('core').controller('customersCtrl', function($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function (response) {$scope.names = response.records;});
+});
+
+
+angular.module('core').controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
