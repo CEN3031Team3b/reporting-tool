5f0e47917dd28754acbdd43e72d1cba3f7447c0b
yane3628 <yane3628@ufl.com>
2015-10-31 17:38:50 -0400

    Successfully found one order from the json response object

--- modules/products/server/controllers/mws.server.controller.js
+++ modules/products/server/controllers/mws.server.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var config = require('../../../../config/env/local');
+    //db = require();
+
 
 var MWS = require('mws-sdk'),
     client = new MWS.Client(config.accessKeyId, config.secretAccessKey, config.merchantId, {}),
@@ -13,7 +15,17 @@ exports.orders = function(request, response) {
 	sf.params.CreatedAfter.value = '2014-07-13';
 	client.invoke(sf, function(RESULT){
   		console.log('--------');
-  		console.log(JSON.stringify(RESULT));
+  		//console.log(JSON.stringify(RESULT)); // displays result to console
   		console.log('--------');
+  		//response.send(RESULT['ListOrdersResult']['Orders']['Order']['LastestShipDate'][0]); // displays result to page
+      //response.send(RESULT);
+      //var parsedData = JSON.parse(RESULT);
+      //var dates = parsedData[0];
+      //console.log(dates);
+      // response.send(RESULT.ListOrdersResponse.ListOrdersResult.Orders.Order['LatestShipDate']);
+      response.send(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[0]);
+      console.log(JSON.stringify(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[0]));
+
+
 	});
 };
