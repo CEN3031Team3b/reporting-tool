1b3e1b0dd0913cbc8109d67ee668a1eb0270f9fd
yane3628 <yane3628@ufl.com>
2015-11-16 18:21:32 -0500

    added for loop to iterate through items in order. added comments

--- modules/products/server/controllers/mws.server.controller.js
+++ modules/products/server/controllers/mws.server.controller.js
@@ -14,63 +14,70 @@ var MWS = require('mws-sdk'),
     marketPlaceId = 'ATVPDKIKX0DER',
     orderID = '';
 
-
+//function that makes call to amws for specified date range
 exports.orders = function(request, response) {
 	var sf = new MWS.Orders.requests.ListOrders({'marketPlaceId': marketPlaceId});
-	sf.params.MarketplaceId.value = marketPlaceId;
+	
+  //assigning values to ensure we only get amazon information with criteria below
+  sf.params.MarketplaceId.value = marketPlaceId;
 	sf.params.CreatedAfter.value = '2014-07-13';
   sf.params.FulfillmentChannel.value = 'AFN';
   sf.params.OrderStatus.value = 'Shipped';
+
+  //making the request to amazon
 	client.invoke(sf, function(RESULT){
-      //response.send(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[0]);
       var i = 0;
 
+      //OUTER LOOP START - looping through all orders in response
       for(i = 0; i < RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order.length; i++) {
 
-        console.log(JSON.stringify(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i])); // returns the first order in response information
+        /* console.log(JSON.stringify(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i])); // returns the first order in response information */
+        
+        //stores order id number so inner loop can get individual products from the order
         orderID = RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i].AmazonOrderId[0];
-        console.log(orderID);
+        /*console.log(orderID);*/
       
-        // api call for the list of items in a particular order
+        // initialize api call for the list of items in a particular order
         var sf1 = new MWS.Orders.requests.ListOrderItems({'orderID': orderID});
         sf1.params.AmazonOrderId.value = orderID;
-        client.invoke(sf1, function(RESULT){
-          //response.send(RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0]);
-          var newSku = RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0].SellerSKU[0],
-              newQty = RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0].QuantityShipped[0],
-              newPrice = RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0].ItemPrice[0].Amount[0];
+        client.invoke(sf1, function(RESULT2){
+          var j = 0;
 
-          //creating new product
-          var newProduct = new product({
-            sku: newSku,
-            quantity: newQty,
-            price: newPrice
-          });
+          //INNER LOOP1 START- looping through all items in each order
+          for(j = 0; j < RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem.length; j++) {
+            var newSku = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].SellerSKU[0],
+                newQty = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].QuantityShipped[0],
+                newPrice = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].ItemPrice[0].Amount[0];
 
-          //NOT WORKING BECAUSE PRODUCTS NEED AT LEAST ONE DOCUMENT
+            //creating new product
+            var newProduct = new product({
+              sku: newSku,
+              quantity: newQty,
+              price: newPrice
+            });
 
-          //saving new product to local database
-          newProduct.save(function (err) {
-            if (err) {
-              return response.status(400).send({
-                message: errorHandler.getErrorMessage(err)
-              });
-            } else {
-              response.json(product);
-            }
-          });
+            // have associated user for this product
+            newProduct.user = request.user;
 
-          //response.send(newProduct);
-          // response.send(qty);
-          // response.send(price);
-          console.log('SKU: ' + newSku);
-          console.log('qty: ' + newQty);
-          console.log('price: ' + newPrice);
-           //RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0] // returns first item of order
+            //saving new product to local database
+            newProduct.save(function (err) {
+              if (err) {
+                return response.status(400).send({
+                  message: errorHandler.getErrorMessage(err)
+                });
+              } else {
+                response.json(product);
+              }
+            });
+
+            console.log('SKU: ' + newSku);
+            console.log('qty: ' + newQty);
+            console.log('price: ' + newPrice);
+            /*RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0] // returns first item of order*/
+          }
+          //INNER LOOP1 END
         });
       }
-
-      
-
+      //OUTER LOOP END
 	});
 };
