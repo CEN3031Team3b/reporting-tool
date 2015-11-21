d6cb94a1475fce5c60dc33b70caa5aa93fadb5e9
reyjr6 <reyolivojr@hotmail.com>
2015-11-21 18:36:45 -0500

    Some improvements on header Setting features
    Still need to make some UI improvements, and make it connect to the
    Database

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -19,5 +19,28 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.$on('$stateChangeSuccess', function () {
       $scope.isCollapsed = false;
     });
+
+    var button = document.getElementById("info");
+var myDiv = document.getElementById("myDiv");
+
+function show() {
+    myDiv.style.visibility = "visible";
+}
+
+function hide() {
+    myDiv.style.visibility = "hidden";
+}
+
+function toggle() {
+    if (myDiv.style.visibility === "hidden") {
+        show();
+    } else {
+        hide();
+    }
+}
+
+hide();
+
+button.addEventListener("click", toggle, false);
   }
 ]);
