0a0b09fcd1e355181a1125fe84ac6a19070bcfb0
yane3628 <yane3628@ufl.com>
2015-11-22 16:38:56 -0500

    orders function moved here and needs to be called only if needed. Started a function to check if the timeframe already exists in our DB

--- modules/products/server/controllers/products.server.controller.js
+++ modules/products/server/controllers/products.server.controller.js
@@ -8,6 +8,11 @@ var path = require('path'),
   product = mongoose.model('product'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
+var config = require('../../../../config/env/local'),
+    MWS = require('mws-sdk'),
+    client = new MWS.Client(config.accessKeyId, config.secretAccessKey, config.merchantId, {}),
+    marketPlaceId = 'ATVPDKIKX0DER';
+
 /**
  * Create a product
  */
@@ -39,8 +44,12 @@ exports.read = function (req, res) {
 exports.update = function (req, res) {
   var product = req.product;
 
-  product.title = req.body.title;
-  product.content = req.body.content;
+  product.sku = req.body.sku;
+  product.quantity = req.body.quantity;
+  product.price = req.body.price;
+  product.purchaseDate = req.body.purchaseDate;
+  product.orderID = req.body.orderID;
+  product.user = req.user;
 
   product.save(function (err) {
     if (err) {
@@ -71,6 +80,96 @@ exports.delete = function (req, res) {
 };
 
 /**
+ * find product by date
+ */
+function productByDate (req, res, next, CreatedAfter, CreatedBefore) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'product is invalid'
+    });
+  }
+
+  product.findById(id).populate('user', 'displayName').exec(function (err, product) {
+    if (err) {
+      return next(err);
+    } else if (!product) {
+      return res.status(404).send({
+        message: 'No product with that identifier has been found'
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+//Makes Amazon MWS API calls when needed
+function orders(request, response){
+    var sf = new MWS.Orders.requests.ListOrders({'marketPlaceId': marketPlaceId});
+  
+  //assigning values to ensure we only get amazon information with criteria below
+  sf.params.MarketplaceId.value = marketPlaceId;
+  sf.params.CreatedAfter.value = '2015-10-20';
+  sf.params.CreatedBefore.value = '2015-11-18';
+  sf.params.FulfillmentChannel.value = 'AFN';
+  sf.params.OrderStatus.value = 'Shipped';
+
+  //making the request to amazon
+  client.invoke(sf, function(RESULT){
+      var i = 0;
+
+      //OUTER LOOP START - looping through all orders in response
+      for(i = 0; i < RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order.length; i++) {
+
+        /* console.log(JSON.stringify(RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i])); // returns the first order in response information */
+        
+        //stores order id number so inner loop can get individual products from the order
+        // stores date order was made
+        var orderID = RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i].AmazonOrderId[0],
+            newDate = RESULT.ListOrdersResponse.ListOrdersResult[0].Orders[0].Order[i].PurchaseDate[0];
+        /*console.log(orderID);*/
+      
+        // initialize api call for the list of items in a particular order
+        var sf1 = new MWS.Orders.requests.ListOrderItems({'orderID': orderID});
+        sf1.params.AmazonOrderId.value = orderID;
+        client.invoke(sf1, function(RESULT2){
+          var j = 0;
+
+          //INNER LOOP1 START- looping through all items in each order
+          for(j = 0; j < RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem.length; j++) {
+            var newSku = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].SellerSKU[0],
+                newQty = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].QuantityShipped[0],
+                newPrice = RESULT2.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[j].ItemPrice[0].Amount[0];
+
+            //creating new product
+            var newProduct = new product({
+              sku: newSku,
+              quantity: newQty,
+              price: newPrice,
+              purchaseDate: newDate,
+              orderID: orderID
+            });
+
+            // have associated user for this product
+            //newProduct.user = request.user;
+
+            //saving new product to local database
+            newProduct.save();
+
+            console.log('SKU: ' + newSku);
+            console.log('qty: ' + newQty);
+            console.log('price: ' + newPrice);
+            console.log('date: ' + newDate);
+            /*RESULT.ListOrderItemsResponse.ListOrderItemsResult[0].OrderItems[0].OrderItem[0] // returns first item of order*/
+          }
+          //INNER LOOP1 END
+        });
+      }
+      //OUTER LOOP END
+  });
+}
+
+/**
  * List of products
  */
 exports.list = function (req, res) {
@@ -83,6 +182,9 @@ exports.list = function (req, res) {
       res.json(products);
     }
   });
+
+  //if date doesn't exist in our DB, call orders with the dates that are missing
+  //orders();
 };
 
 /**
