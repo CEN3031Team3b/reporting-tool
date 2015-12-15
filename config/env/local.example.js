9ef2148bc8004603c0d119f29647ff893471f748
yane3628 <yane3628@gmail.com>
2015-12-15 16:08:22 -0500

    now has instructions on how to make local.js file

--- config/env/local.example.js
+++ config/env/local.example.js
@@ -2,22 +2,14 @@
 
 // Rename this file to local.js for having a local configuration variables that
 // will not get commited and pushed to remote repositories.
-// Use it for your API keys, passwords, etc.
+// Add your Amazon MWS credentials to the fields below
 
 /* For example:
 
 module.exports = {
-  db: {
-    uri: 'mongodb://localhost/local-dev',
-    options: {
-      user: '',
-      pass: ''
-    }
-  },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/auth/facebook/callback'
-  }
+  accessKeyId: '*****', 
+  secretAccessKey: '*****', 
+  merchantId: '*****'
 };
+
 */
